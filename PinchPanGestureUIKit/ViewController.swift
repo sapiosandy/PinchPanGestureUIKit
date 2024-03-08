af2e78083ee18ca9f284462b894cb5ec99105fdf
Sandy Gomez <sgomez1029@gmail.com>
2024-03-07 18:46:37 -0800

    Pan, Pinch, and Rotate Gestures in UIKit (UCSD Extension)

--- PinchPanGestureUIKit/ViewController.swift
+++ PinchPanGestureUIKit/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        myImageView.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    @IBAction func pinchGesture(_ sender: UIPinchGestureRecognizer) {
+        myImageView.transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
+    }
+    
+    @IBAction func rotateGesture(_ sender: UIRotationGestureRecognizer) {
+        myImageView.transform = CGAffineTransform(rotationAngle: sender.rotation)
+    }
+    
 }
 
